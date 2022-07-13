280b1e4bc396ddeff4a32ed2b929b6c5ac30581f
Michael Daley <daleyhockey9@gmail.com>
2022-07-13 02:11:02 -0400

    Did some styling created components, etc
    slowly but surely getting the hang of react-native :)

--- ChipInApp/components/AllChips.tsx
+++ ChipInApp/components/AllChips.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button, ScrollView } from 'react-native';
+
+
 
 const Chip = (props: any) =>  {
     //distance
@@ -8,25 +10,63 @@ const Chip = (props: any) =>  {
     // score: string = 'birdie';
 
     return (
-        <View> 
-            <Text>Chip In # {props.number}</Text>
-            <Text>Club: {props.club}</Text>
-            <Text>Yards: {props.distance}</Text>
-            <Text>Course: {props.course}</Text>
-            <Text>For: {props.score}</Text>
-            <Text>Edit Button</Text>
+        <View style={styles.chip}>
+            <View>
+                <Text style={styles.text}>Chip In # {props.number}</Text>
+                <Text style={styles.text}>Club: {props.club}</Text>
+                <Text style={styles.text}>Yards: {props.distance}</Text>
+                <Text style={styles.text}>Course: {props.course}</Text>
+                <Text style={styles.text}>For: {props.score}</Text>
+            </View>
+            <Button title="Edit" onPress={() => handleEdit(props.number)}/>
         </View>
     );
 };
 
+const handleEdit = (id: number) => {
+    console.log("edit " + id);
+}
+
+//need to use a real db eventually
+var chips = [<Chip key="1" number={1} club='60 degree' distance={87} course='Shannopin' score='Birdie (3)'/>,
+             <Chip key="2" number={2} club='54 degree' distance={18} course='South Park' score='Birdie (4)'/>,
+             <Chip key="3" number={3} club='54 degree' distance={12} course='Shannopin' score='Par (4)'/>,
+             <Chip key="4" number={4} club='60 degree' distance={25} course='Moon' score='Birdie (2)'/>,
+             <Chip key="5" number={5} club='60 degree' distance={43} course='Shannopin' score='Bogey (5)'/>,
+             <Chip key="6" number={6} club='p wedge' distance={19} course='Grandview' score='Eagle (3)'/>,
+             <Chip key="7" number={7} club='a wedge' distance={32} course='Westwood' score='Birdie (4)'/>,
+             <Chip key="8" number={8} club='60 degree' distance={104} course='Ponderosa' score='Par (5)'/>,
+             <Chip key="9" number={9} club='60 degree' distance={19} course='Shannopin' score='Birdie (3)'/>];
+
+
+
 const AllChips = () => {
-        return (
-            <View>
-                <Chip number={1} club='60 degree' distance={87} course='Shannopin' score='birdie'/>
-                <Chip number={2} club='54 degree' distance={13} course='Moon' score='bogey'/>
-                <Chip/>
-            </View>
-        );
+    const num_chips = chips.length;
+    return (
+        <ScrollView style={styles.allChips}>
+            {chips}
+        </ScrollView>
+    );
 };
+
+
+const styles = StyleSheet.create({
+    chip: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'baseline',
+        //   justifyContent: 'center',
+        padding: 2,
+        borderWidth:1,
+        width:'100%'
+    },
+    allChips:{
+        width: '100%',
+    },
+    text:{
+        fontSize:15,
+    }
+  });
 
 export default AllChips;
--- ChipInApp/screens/Home.tsx
+++ ChipInApp/screens/Home.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Button, FlatList, Pressable, StyleSheet } from 'react-native';
 import AllChips from '../components/AllChips';
-import ChipTotals from '../components/ChipTotals';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -11,26 +10,59 @@ export default function Home({ navigation }: RootTabScreenProps<'Home'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Chip Ins</Text>
-      <AllChips></AllChips>
-      <ChipTotals total={3} longest={87}></ChipTotals>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <AllChips></AllChips>
+        <View style={{flexDirection: 'row',borderWidth:2,minWidth:'100%',justifyContent:'space-around'}}>
+        <ChipTotals total={9} longest={104}></ChipTotals>{/*Need these to be real*/}
+        <Button title="Add Chip" onPress={() => navigation.navigate('Modal')}/>
+        </View>
     </View>
+  );
+}
+
+const ChipTotals = (props: any) => {
+  return (
+      <View> 
+          <Text>Total: {props.total}</Text>
+          <Text>Longest: {props.longest}</Text>
+      </View>
+  );
+};
+
+// const AddChip = ({navigation}) => {
+//   //use state hooks here
+//   return (
+//       // <Button title="Add Chip In" onPress={handleAddChip}/>
+//       <Button title="Add Chip"
+//       onPress={() =>
+//         navigation.navigate('Profile', { name: 'Jane' })
+//       }/>
+//   );
+// };
+
+const handleAddChip = () =>{
+  console.log("add chip")
+  return (
+    <Text>Fucks</Text>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
   title: {
+    width: '100%',
+    textAlign: 'center',
+    justifyContent: 'center',
     fontSize: 20,
     fontWeight: 'bold',
+    color:'blue',
+    borderWidth: 2,
+    borderColor:'lightblue',
+    padding:2,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  chip:{
   },
 });
